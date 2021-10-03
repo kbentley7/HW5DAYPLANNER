279976cd6c6ae16d830fbd8f71f482db48d2ce53
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-03 00:38:31 -0400

    edited index.html and script.js

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ $(Window).on("load", function () {
     currentDate = monment().format("add MMM Do YYYY, h:m a");
     $("currentDay").append(currentDate);
     currentTime = moment().format("H");
+
+    function renderAppointments() {
+      storedAppointments = JSON.parse(localStorage.getItem("appointments"));
+      if (storedAppointments !== null) {
+          for (i = 0; i < storedAppointments.length; i++) {
+              returnedAppointments = storedAppointments[i];
+              details = returnedAppointments.details;
+              timeIndex = returnedAppointments.time;
+              timeIndex = timeIndex.replace(":00", '');
+              if (details !== null) {
+                  $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
+              }
+          }
+      }
+  }
+
+
+
+
+
 })
   
 
